3ddef3d334727f81912109cc1ce486ee96e31c19
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-08-29 02:35:21 +0600

    api ... more

--- js/meal.js
+++ js/meal.js
@@ -1,8 +1,8 @@
 const loadMeals = (search) => {
    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
    fetch(url)
-   .then(res => res.json())
-   .then(data => displayMeals(data.meals))
+      .then(res => res.json())
+      .then(data => displayMeals(data.meals))
 }
 
 const displayMeals = meals => {
@@ -13,7 +13,7 @@ const displayMeals = meals => {
       const mealDiv = document.createElement('div');
       mealDiv.classList.add('col');
       mealDiv.innerHTML = `
-         <div class="card">
+         <div class="card" onclick="loadMealDetail(${meal.idMeal})">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="Image">
             <div class="card-body">
                <h5 class="card-title">${meal.strMeal}</h5>
@@ -31,5 +31,46 @@ const searchFood = () => {
    searchInput.value = '';
    loadMeals(searchText);
 }
+
+
+// const loadMealDetail = (idMeal) => {
+//    // console.log(idMeal);
+//    const url2 = `https://www.themealdb.com/api/json/v1/1/search.php?i=${idMeal}`;
+//    // console.log(url2);
+
+//    fetch(url2)
+//       .then(res => res.json())
+//       .then(data => console.log(data))
+
+// }
+
+const loadMealDetail = (idMeal) => {
+   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+   // console.log(url);
+
+   fetch(url)
+   .then(res => res.json())
+   .then(data => displayMealDetail(data.meals[0]));
+}
+
+
+const displayMealDetail = (meal) => {
+   const detailedContainer = document.getElementById('meal-deatils');
+   detailedContainer.innerHTML = '';
+   const mealDiv = document.createElement('div');
+   mealDiv.classList.add('card,mb-3');
+   mealDiv.innerHTML = `
+      <img src="${meal.strMealThumb}" class="img-fluid" alt="image">
+      <div class="card-body">
+         <h5 class="card-title">${meal.strMeal}</h5>
+         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.</p>
+         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+   `
+   detailedContainer.appendChild(mealDiv);
+
+}
+
 
 loadMeals('')
---
+++ js/user.js
@@ -0,0 +1,23 @@
+const loadUserFetcher = () => {
+   const url = `https://randomuser.me/api/?gender=female`;
+   fetch(url)
+      .then((response) => response.json())
+      .then(data => displayUser(data.results[0]))
+      .catch((error) => console.log(error))
+}
+
+const loadUserAsync = async () => {
+   const url = `https://randomuser.me/api/?gender=female`;
+   try {
+      const res = await fetch(url);
+      const data = await res.json();
+      displayUser(data.results[0]);
+   }
+   catch (error) {
+      console.log(error);
+   }
+}
+
+const displayUser = (user) => {
+   console.log(user);
+}
